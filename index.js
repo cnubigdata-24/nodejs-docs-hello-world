e7c8a12ad54309f4862344775dd53cf6e40e32bd
Paul's Repository <cnubigdata@gmail.com>
2025-10-25 15:04:08 +0900

    Update version number in Hello World response

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const router = express.Router();
 
 // Hello World for index page
 app.get('/', function (req, res) {
-    return res.send("Hello World! Current Version 1.4. !!");
+    return res.send("Hello World! Current Version 2.0. !!");
 })
 
 app.get('/api', function (req, res) {
